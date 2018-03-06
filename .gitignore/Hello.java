9cab45fd9ab833bda0daaa51d40a2f1fa526e12a
narcisa001 <narcisasnep@yahoo.com>
2018-03-06 07:09:49 +0200

    Create Hello.java

---
+++ .gitignore/Hello.java
@@ -0,0 +1,9 @@
+import java.util.*;
+ 
+    public class Hello {
+        public static void main(String[] args) {
+        System.out.println("Hello, the current date is: ");
+        Date data = new Date();
+        System.out.println(data);
+    }
+} 
